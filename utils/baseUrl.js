382d50c8fe703f87a3171299774a827d2b618a44
qwerty1249 <122226253+qwerty1249@users.noreply.github.com>
2023-04-12 04:00:27 +0530

    Update baseUrl.js

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://dailyjournalviewdraft.vercel.app";
+    : "https://main--astonishing-salmiakki-44f443.netlify.app";
 
 module.exports = baseUrl;
